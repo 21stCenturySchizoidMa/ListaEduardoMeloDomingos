080e72b1193662e04be53295f047312cc8d421b9
ruan linhares <ruanvitor0941@gmail.com>
2024-09-05 18:04:25 -0300

    atividades de lenin

--- src/LIsta01/Exercicio09.java
+++ src/LIsta01/Exercicio09.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 public class Exercicio09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("insira sua idade em anos: ");
+        int idadeAnos = sc.nextInt();
 
+        int idadeDias = idadeAnos * 365;
+        System.out.println("sua idade em dias é " + idadeDias);
+
+        sc.close();
     }
 }
